5c6ddd36f38ad9a3ec16048b33abc2126822f95f
Anubhav0103 <62379559+Anubhav0103@users.noreply.github.com>
2024-12-29 11:16:31 +0530

    Add files via upload

--- src/routes/signupRoutes.js
+++ src/routes/signupRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const signupController = require('../controller/signupController');
+const signupController = require('../controllers/signupController');
 
 const router = express.Router();
 
