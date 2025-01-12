05f4750351f3cc77dcd833eba9911659a07331ec
Anubhav0103 <62379559+Anubhav0103@users.noreply.github.com>
2025-01-12 20:02:30 +0530

    Delete src directory

--- src/routes/signupRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const signupController = require('../controllers/signupController');
-
-const router = express.Router();
-
-router.post('/', signupController.handleSignup);
-
-module.exports = router;
